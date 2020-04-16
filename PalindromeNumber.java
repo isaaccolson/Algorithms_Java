5d290e2791ad855c0f9703e189cd96efd6ff3c0e
isaaccolson <isaac.colson@gmail.com>
2020-04-16 09:51:54 -0400

    Update PalindromeNumber.java

--- PalindromeNumber.java
+++ PalindromeNumber.java
@@ -3,10 +3,10 @@ package com.company;
 public class PalindromeNumber {
     public boolean isPalindrome(int x) {
 
-        String sX = Integer.toString(x);
+        String s = Integer.toString(x);
 
-        for (int i = 0, length = sX.length(); i < length / 2; i++) {
-            if (sX.charAt(i) != sX.charAt(length-i-1)) {
+        for (int i = 0, length = s.length(); i < length / 2; i++) {
+            if (s.charAt(i) != s.charAt(length - (i + 1))) {
                 return false;
             }
         }
